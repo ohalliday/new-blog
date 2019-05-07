57199881c40ae38ea07e550fa30cb023a9a6f2b1
Owen Halliday <owenhalliday1@gmail.com>
2019-05-07 14:43:32 +0100

    Blog Created

---
+++ src/website-config.ts
@@ -0,0 +1,62 @@
+export interface WebsiteConfig {
+  title: string;
+  description: string;
+  coverImage: string;
+  logo: string;
+  /**
+   * Specifying a valid BCP 47 language helps screen readers announce text properly.
+   * See: https://dequeuniversity.com/rules/axe/2.2/valid-lang
+   */
+  lang: string;
+  /**
+   * blog full path, no ending slash!
+   */
+  siteUrl: string;
+  /**
+   * full url, no username
+   */
+  facebook?: string;
+  /**
+   * full url, no username
+   */
+  twitter?: string;
+  /**
+   * hide or show all email subscribe boxes
+   */
+  showSubscribe: boolean;
+  /**
+   * create a list on mailchimp and then create an embeddable signup form. this is the form action
+   */
+  mailchimpAction?: string;
+  /**
+   * this is the hidden input field name
+   */
+  mailchimpName?: string;
+  /**
+   * name and id of the mailchimp email field
+   */
+  mailchimpEmailFieldName?: string;
+  /**
+  /**
+   * Appears alongside the footer, after the credits
+   */
+  footer?: string;
+}
+
+const config: WebsiteConfig = {
+  title: 'Owen Halliday',
+  description: '17 y/o Software Developer.',
+  coverImage: 'img/blog-cover.jpg',
+  logo: 'img/colour-logo.png',
+  lang: 'en',
+  siteUrl: 'https://owenhalliday.co.uk',
+  facebook: 'https://www.facebook.com/owenhalliday.co.uk',
+  twitter: 'https://twitter.com/owenhalliday1',
+  showSubscribe: true,
+  mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+  mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+  mailchimpEmailFieldName: 'MERGE0',
+  footer: '',
+};
+
+export default config;
