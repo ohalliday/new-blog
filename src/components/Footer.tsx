136f1cb4e57993ea30591201f5e65cc6b2211fa6
Owen Halliday <owenhalliday1@gmail.com>
2019-05-08 11:24:55 +0100

    Deleted undeclared imports

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,8 @@
 import { Link } from 'gatsby';
-import { setLightness } from 'polished';
 import * as React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 
-import { colors } from '../styles/colors';
 import { outer, inner } from '../styles/shared';
 import config from '../website-config';
 
